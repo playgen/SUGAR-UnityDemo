b1e5f5b4cd96ee1f111ee706262e8f51731c74a6
Ben Hirsh <ben@playgen.com>
2016-07-27 15:21:07 +0100

    Build script for WebGl

---
+++ Assets/Editor/Build/BuildWebGL.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class BuildWebGL : MonoBehaviour
+{
+
+	[MenuItem("Tools/WebGL Build")]
+	static void Build()
+	{
+		string[] scenes = { "Assets/Scenes/setup.unity", "Assets/Scenes/demo.unity" };
+		//EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.WebGL);
+		EditorUserBuildSettings.webGLOptimizationLevel = 3;
+		PlayerSettings.apiCompatibilityLevel = ApiCompatibilityLevel.NET_2_0;
+		BuildPipeline.BuildPlayer(scenes, @"Build/WebGL", BuildTarget.WebGL, BuildOptions.None);
+	}
+
+}
