31fa0906bd19622bf2b69428a8b54f4f720399e2
Jared <jared@playgen.com>
2016-11-18 14:25:06 +0000

    unity 5.4 compatability

--- Assets/Editor/Build/BuildWebGL.cs
+++ Assets/Editor/Build/BuildWebGL.cs
@@ -9,7 +9,7 @@ public class BuildWebGL : MonoBehaviour
 	{
 		string[] scenes = { "Assets/Scenes/setup.unity", "Assets/Scenes/demo.unity" };
 		//EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.WebGL);
-		EditorUserBuildSettings.webGLOptimizationLevel = 3;
+		//EditorUserBuildSettings.webGLOptimizationLevel = 3; //todo was commented for unity 5.4.2 compatability but haven't checked for replacement functionality
 		PlayerSettings.apiCompatibilityLevel = ApiCompatibilityLevel.NET_2_0;
 		BuildPipeline.BuildPlayer(scenes, @"Build/WebGL", BuildTarget.WebGL, BuildOptions.None);
 	}
