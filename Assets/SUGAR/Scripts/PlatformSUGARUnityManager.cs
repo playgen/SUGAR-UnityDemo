6c4c9b50d1dc9e56b264aee83b3fe92853749447
Jared <jaredgglass@gmail.com>
2018-06-28 15:17:55 +0100

    using PlatformSUGARUnityManager to init WebGL specific client

---
+++ Assets/SUGAR/Scripts/PlatformSUGARUnityManager.cs
@@ -0,0 +1,17 @@
+﻿using PlayGen.SUGAR.Client;
+using PlayGen.SUGAR.Unity;
+#if UNITY_WEBGL
+using PlayGen.SUGAR.Unity.WebGL;
+#endif
+
+public class PlatformSUGARUnityManager : SUGARUnityManager
+{
+    protected override SUGARClient CreateSUGARClient(string baseAddress)
+    {
+#if UNITY_WEBGL && !UNITY_EDITOR
+        return new SUGARClient(baseAddress, new UnityWebGlHttpHandler(), false);
+#else
+        return base.CreateSUGARClient(baseAddress);
+#endif
+    }
+}
